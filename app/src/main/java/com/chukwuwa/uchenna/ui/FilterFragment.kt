444192922d480024dd04faaceb52b9502768ff69
Dablazer-ufc <ChukwuwaUchenna@gmail.com>
2020-02-06 10:29:48 +0100

    Setup the ui with fragments

---
+++ app/src/main/java/com/chukwuwa/uchenna/ui/FilterFragment.kt
@@ -0,0 +1,26 @@
+package com.chukwuwa.uchenna.ui
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.chukwuwa.uchenna.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FilterFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_filter, container, false)
+    }
+
+
+}
