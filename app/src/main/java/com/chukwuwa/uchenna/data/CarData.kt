a837061a98cfe838db2e5ec341d5b331ba3f8aa4
Dablazer-ufc <ChukwuwaUchenna@gmail.com>
2020-02-06 15:26:34 +0100

    Replace the Non functional dependency for reading excel files and hook up retrofit
    the previous dependency was not able to carry out the laid out functions properly, and retrofit is the best way with working with REST API

---
+++ app/src/main/java/com/chukwuwa/uchenna/data/CarData.kt
@@ -0,0 +1,17 @@
+package com.chukwuwa.uchenna.data
+
+data class CarData(
+    var id: Int,
+    var first_name: String,
+    var last_name: String,
+    var email: String,
+    var country: String,
+    var car_model: String,
+    var car_model_year: Int,
+    var car_color: String,
+    var gender: String,
+    var job_title: String,
+    var bio: String
+) {
+
+}
